45bd8384b9040dd5864aa1d0c692fe8ab97403c9
CoolD69 <parthasingharoy166@gmail.com>
2022-08-15 12:33:58 +0530

    Enter page ready

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,14 +1,22 @@
+let video
+
 function setup() {
-    noCanvas();
-    const video = createCapture(VIDEO);
-    video.size(160, 120);
+    createCanvas(400, 700);
+    video = createCapture(VIDEO);
+    // video.size(300, 300);
+    video.hide()
     let lat, lon;
+
     const button = document.getElementById('submit');
+
     button.addEventListener('click', async event => {
         const mood = document.getElementById('mood').value;
+        const name = document.getElementById('name').value;
+        const desc = document.getElementById('desc').value;
         video.loadPixels();
         const image64 = video.canvas.toDataURL();
-        const data = { lat, lon, mood, image64 };
+        const data = { lat, lon,name, mood,desc, image64 };
+        // console.log(data);
         const options = {
             method: 'POST',
             headers: {
@@ -22,11 +30,11 @@ function setup() {
     });
 
     if ('geolocation' in navigator) {
-        console.log('geolocation available');
+        // console.log('geolocation available');
         navigator.geolocation.getCurrentPosition(position => {
             lat = position.coords.latitude;
             lon = position.coords.longitude;
-            console.log(lat, lon);
+            // console.log(lat, lon);
             document.getElementById('latitude').textContent = lat;
             document.getElementById('longitude').textContent = lon;
         });
@@ -34,3 +42,19 @@ function setup() {
         console.log('geolocation not available');
     }
 }
+
+
+function draw() {
+
+
+    image(video, 0, 0, width, width * video.height / video.width);
+    var can = document.getElementById('defaultCanvas0')
+
+    let video123 = document.getElementById('video123').appendChild(can)
+    can.style.position = 'absolute';
+    can.style.top = "10px";
+    can.style.left = "10px";
+    can.style.borderRadius = "10px";
+
+}
+
