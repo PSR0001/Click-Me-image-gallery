681722fa35fb5396bf221cf3258d3ce7ed4e9e94
CoolD69 <parthasingharoy166@gmail.com>
2022-08-15 10:27:48 +0530

    create all file

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-   const express = require('express');
-   const app = express();
-
-   // define the port and the global variable
-   
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000 //if deploy on cloud
 
 
+app.use(express.static('public'))
 
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-   app.listen()
-
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
 
 
 
